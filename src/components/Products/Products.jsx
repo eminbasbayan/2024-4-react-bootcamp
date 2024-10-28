3dba23454ee6ee87f69357454032dfaa39b70a29
eminbasbayan <emin.basbayan@icloud.com>
2024-10-28 23:37:33 +0300

    react map kullanımı

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,29 +2,18 @@ import { productsData } from "../../productsData";
 import ProductItem from "./ProductItem";
 import "./Products.css";
 
-
-
 function Products() {
-
   return (
     <div className="products">
       <h2>Products</h2>
       <div className="products-wrapper">
-        <ProductItem
-          image={productsData[0].image}
-          title={productsData[0].title}
-          price={productsData[0].price}
-        />
-        <ProductItem
-          image={productsData[1].image}
-          title={productsData[1].title}
-          price={productsData[1].price}
-        />
-        <ProductItem
-          image={productsData[2].image}
-          title={productsData[2].title}
-          price={productsData[2].price}
-        />
+        {productsData.map((product) => {
+          return <ProductItem
+            image={productsData[0].image}
+            title={productsData[0].title}
+            price={productsData[0].price}
+          />;
+        })}
       </div>
     </div>
   );
