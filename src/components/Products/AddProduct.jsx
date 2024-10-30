af264614a50657599133641ed4eae0d2bf930139
eminbasbayan <emin.basbayan@icloud.com>
2024-10-30 22:44:52 +0300

    tek state ve tek function ile form yönetimi

--- src/components/Products/AddProduct.jsx
+++ src/components/Products/AddProduct.jsx
@@ -1,20 +1,42 @@
+import { useState } from "react";
 import Button from "../UI/Button";
 import "./AddProduct.css";
 
 function AddProduct() {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+
+  function handleTitleChange(event) {
+    setTitle(event.target.value);
+  }
+
+  function handlePriceChange(event) {
+    setPrice(event.target.value);
+  }
+
+  function handleImageChange(event) {
+    setImage(event.target.value);
+  }
+
   return (
     <form className="add-product-form">
+      {title}
+      <br />
+      {price}
+      <br />
+      {image}
       <label>
         Title:
-        <input type="text" name="title" required />
+        <input type="text" onChange={handleTitleChange} />
       </label>
       <label>
         Price:
-        <input type="number" name="price" required />
+        <input type="number" onChange={handlePriceChange} />
       </label>
       <label>
         Image URL:
-        <input type="text" name="imageUrl" required />
+        <input type="text" onChange={handleImageChange} />
       </label>
       <Button color={"success"}>Yeni Ürün Ekle</Button>
     </form>
