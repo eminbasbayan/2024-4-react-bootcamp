e10e5b83098117f14b0a72ef88736b2dd0a188bd
eminbasbayan <emin.basbayan@icloud.com>
2024-11-06 22:10:25 +0300

    clean-up function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Counter from './components/Counter';
 import Products from './components/Products/Products';
 
 function App() {
   return (
     <div className="app">
       <div className="container mx-auto">
-        <Counter />
         <Products />
       </div>
     </div>
