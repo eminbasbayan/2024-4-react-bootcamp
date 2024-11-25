6ffbda2faa26b9ca32f8f7d94b4d027b83844400
eminbasbayan <emin.basbayan@icloud.com>
2024-11-25 22:48:23 +0300

    updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,20 +7,21 @@ const Header = () => {
   const { cartItems } = useCart();
   const sepetUrunSayisi = cartItems.length;
 
-  console.log(cartItems);
-
   return (
     <header className="header">
       <div className="container mx-auto px-4">
         <nav className="flex items-center justify-between h-16">
           <Link to="/" className="text-xl font-bold">
-            E-Ticaret
+           Benim E-Ticaret
           </Link>
 
           <div className="flex items-center gap-6">
             <Link to="/" className="nav-link">
               Ürünler
             </Link>
+            <a href="/" className="nav-link">
+              Ürünler
+            </a>
             <Link to="/sepet" className="nav-link cart-link">
               Sepet ({sepetUrunSayisi})
             </Link>
