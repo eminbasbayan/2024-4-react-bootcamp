e3870226f1efd562f8b876eb4c1a774abeeb82f5
eminbasbayan <emin.basbayan@icloud.com>
2024-11-27 21:31:15 +0300

    useLocation hook

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../components/Header/Header';
 
 const MainLayout = () => {
+  const location = useLocation();
+
   return (
     <div className="main-layout">
-      <Header />
+      {!location.pathname.startsWith('/auth') && <Header />}
       <div className="content">
         <Outlet />
       </div>
