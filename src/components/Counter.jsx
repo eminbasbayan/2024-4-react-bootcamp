759fe74c16a181deb8450e2951f4173909c76251
eminbasbayan <emin.basbayan@icloud.com>
2024-12-02 21:47:47 +0300

    redux toolkit kurulumu - counter slice yapısı

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,36 +1,11 @@
-import { useEffect, useState } from 'react';
 import Button from './UI/Button';
 
 function Counter() {
-  const [count, setCount] = useState(0);
-  const [title, setTitle] = useState('Ahmet');
-
-  useEffect(() => {
-    console.log('component ilk yüklendiğinde çalışır!');
-  }, []);
-
-  useEffect(() => {
-    console.log('component dependency list`e bağlı güncellendiğinde çalışır!');
-  }, [title]);
-
-  useEffect(() => {
-    console.log('component her güncellendiğinde çalışır!');
-  });
-
   return (
     <div className="counter">
-      <Button color={'success'} onClick={() => setCount(count + 1)}>
-        Arttır
-      </Button>
-      <p className="text-3xl">{count}</p>
-      <Button color={'danger'} onClick={() => setCount(count - 1)}>
-        Azalt
-      </Button>
-      <br />
-      <Button color={'primary'} onClick={() => setTitle('Emin')}>
-        İsmi Değiştir
-      </Button>
-      <p>{title}</p>
+      <Button color={'success'}>Arttır</Button>
+      <p className="text-3xl">{0}</p>
+      <Button color={'danger'}>Azalt</Button>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
+import Counter from '../components/Counter';
 import Products from '../components/Products/Products';
 
 const HomePage = () => {
   return (
     <div className="home-page">
       <div className="container mx-auto px-4 py-8">
+        <Counter />
         <Products />
       </div>
     </div>
