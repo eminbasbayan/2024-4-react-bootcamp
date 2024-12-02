a4ef6a379daee6b6e7717a603cc953ad91b1ac02
eminbasbayan <emin.basbayan@icloud.com>
2024-12-02 22:17:14 +0300

    useSelector hook

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,13 @@
+import { useSelector } from 'react-redux';
 import Button from './UI/Button';
 
 function Counter() {
+  const { count } = useSelector((state) => state.counter);
+
   return (
     <div className="counter">
       <Button color={'success'}>Arttır</Button>
-      <p className="text-3xl">{0}</p>
+      <p className="text-3xl">{count}</p>
       <Button color={'danger'}>Azalt</Button>
     </div>
   );
