b4a8fe9de975767ffa1f1f6aa37112f358e5674a
eminbasbayan <emin.basbayan@icloud.com>
2024-12-05 00:05:25 +0300

    .env file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDRs02uF-yi5R7x-dJd_GtgIP1CdoYThY4',
-  authDomain: 'react-24-4-e-commerce.firebaseapp.com',
-  projectId: 'react-24-4-e-commerce',
-  storageBucket: 'react-24-4-e-commerce.firebasestorage.app',
-  messagingSenderId: '760786932724',
-  appId: '1:760786932724:web:a770ee6b95ae86aad55fa1',
-  measurementId: 'G-8HTG5LZBC3',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
