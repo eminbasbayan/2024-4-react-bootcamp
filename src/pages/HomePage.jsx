dbebd83c721047535974f1a9381eab16598107ee
eminbasbayan <emin.basbayan@icloud.com>
2024-12-18 21:33:36 +0300

    optimistic updates and rollbacks

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
+import OptimisticUpdatesAndRollbacks from '../components/OptimisticUpdatesAndRollbacks';
 import Products from '../components/Products/Products';
 
 const HomePage = () => {
   return (
     <div className="home-page">
       <div className="container mx-auto px-4 py-8">
+        <OptimisticUpdatesAndRollbacks />
         <Products />
       </div>
     </div>
