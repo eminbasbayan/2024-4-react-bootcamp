bb33fc1076f69249c1a0021c16811eb5e43cea16
eminbasbayan <emin.basbayan@icloud.com>
2024-12-18 23:23:20 +0300

    useMemo hook kullanımı

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import MyButton from '../components/PerformanceOptimization/MyButton';
 import MyElement from '../components/PerformanceOptimization/MyElement';
+import MyList from '../components/PerformanceOptimization/MyList';
 
 const HomePage = () => {
   const [toggleParagraph, setToggleParagraph] = useState(false);
@@ -11,12 +12,15 @@ const HomePage = () => {
     setToggleParagraph((prevState) => !prevState);
   }, []);
 
+  const listItems = useMemo(()=> [1, 2, 3, 4, 5], [])
+
   return (
     <div className="home-page">
       <div className="container mx-auto px-4 py-8">
         <h1>Hello</h1>
         <MyElement show={false} />
         <MyButton onClick={toggleParagraphHandler}>Toggle Paragraph</MyButton>
+        <MyList items={listItems} />
       </div>
     </div>
   );
