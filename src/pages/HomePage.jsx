5f0462e899849fa12f99f8165c47205d5d75cf49
eminbasbayan <emin.basbayan@icloud.com>
2024-12-18 23:37:25 +0300

    useRef hook kullanımı

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,13 @@
-import { useCallback, useMemo, useState } from 'react';
-import MyButton from '../components/PerformanceOptimization/MyButton';
-import MyElement from '../components/PerformanceOptimization/MyElement';
-import MyList from '../components/PerformanceOptimization/MyList';
+import InputFocusRefExample from "../components/InputFocusRefExample";
+
 
 const HomePage = () => {
-  const [toggleParagraph, setToggleParagraph] = useState(false);
 
-  console.log('HomePage çalıştı!');
 
-  const toggleParagraphHandler = useCallback(() => {
-    setToggleParagraph((prevState) => !prevState);
-  }, []);
-
-  const listItems = useMemo(()=> [1, 2, 3, 4, 5], [])
 
   return (
     <div className="home-page">
-      <div className="container mx-auto px-4 py-8">
-        <h1>Hello</h1>
-        <MyElement show={false} />
-        <MyButton onClick={toggleParagraphHandler}>Toggle Paragraph</MyButton>
-        <MyList items={listItems} />
-      </div>
+      <InputFocusRefExample />
     </div>
   );
 };
